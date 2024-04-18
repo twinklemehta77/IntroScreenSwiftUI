58afcd99069dd412b0dff4ff0218ac95ed1a6b4f
twinklemehta77 <er.twinklemehta@gmail.com>
2024-04-18 11:29:15 -0400

    First Commit

---
+++ IntroScreenSwiftUI/IntroScreenSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroScreenSwiftUIApp.swift
+//  IntroScreenSwiftUI
+//
+//  Created by Twinkle Mehta on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct IntroScreenSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
